669bd2033de3cf03f2b514130a0b518d6204d327
Rodolfo Pelarin <rcpelarin@hotmail.com>
2022-02-13 19:48:10 -0300

    teste

--- Testes/Teste_tinnova.py
+++ Testes/Teste_tinnova.py
@@ -1,18 +1,31 @@
+class DadosEleicao:
+    """
+    Classe onde todos os cálculos para contagem de votos da eleição é definido.
+    """
+    def __init__(self, eleitores, votos_validos, votos_brancos, votos_nulos):
+        self.eleitores = eleitores
+        self.votos_validos = votos_validos
+        self.votos_brancos = votos_brancos
+        self.votos_nulos = votos_nulos
+
+    def validos(self):
+        v = votos_validos/eleitores*100
+        return (v)
+
+    def brancos(self):
+        b = votos_brancos/eleitores*100
+        return (b)
+
+    def nulos(self):
+        n = votos_nulos/eleitores*100
+        return (n)
+
 eleitores = 1000
-validos = 800
+votos_validos = 800
 votos_brancos = 150
 votos_nulos = 50
 
 print(f'O Total de Eleitores é {eleitores}')
-
-#Calculando e apresentando os votos válidos
-v = validos/eleitores*100
-print(f'O percentual de votos válidos é {v}%, sendo {validos} votos')
-
-#Calculando e apresentando os votos em branco
-b = votos_brancos/eleitores*100
+print(f'O percentual de votos válidos é {v}%, sendo {votos_validos} votos')
 print(f'O percentual de votos em branco é de {b}%, sendo {votos_brancos} votos')
-
-#Calculando e apresentando os votos nulos
-n = votos_nulos/eleitores*100
 print(f'O percentual de votos nulos é de {n}%, sendo {votos_nulos} votos')
